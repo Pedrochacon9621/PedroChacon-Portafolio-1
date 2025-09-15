3f149a57feea80613a93c18ef7621e4b5392c6b8
Pedro Chacón <pedrochacon9621@gmail.com>
2025-09-15 22:34:14 +0200

    o5

--- api/index.php
+++ api/index.php
@@ -1,30 +1,20 @@
 <?php
-echo '<pre>';
-echo 'REQUEST_URI: ' . $_SERVER['REQUEST_URI'] . "\n";
-echo 'PATH_INFO: ' . ($_SERVER['PATH_INFO'] ?? 'N/A') . "\n";
-echo 'SCRIPT_NAME: ' . $_SERVER['SCRIPT_NAME'] . "\n";
-echo 'PHP_SELF: ' . $_SERVER['PHP_SELF'] . "\n";
-echo '</pre>';
-
-
-
-require __DIR__ . '/../index.php';
-// Extrae la ruta solicitada
 $ruta = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 // Ruta base del proyecto
 $base = __DIR__ . '/..';
 
-// Verifica si el archivo solicitado existe
+// Verifica si la ruta corresponde a una página específica
 $archivoSolicitado = $base . $ruta;
 
-if (file_exists($archivoSolicitado) && pathinfo($archivoSolicitado, PATHINFO_EXTENSION) === 'php') {
+if (
+    file_exists($archivoSolicitado) &&
+    pathinfo($archivoSolicitado, PATHINFO_EXTENSION) === 'php'
+) {
     require $archivoSolicitado;
-    exit;
+} else {
+    require $base . '/index.php';
 }
-
-echo '<pre>Ruta limpia: ' . $ruta . '</pre>';
-
 ?>
 
 
