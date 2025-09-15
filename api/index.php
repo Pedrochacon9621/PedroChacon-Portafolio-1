b8dd7a1f85027e74f11c0b759a498714b2d47a96
Pedro Chacón <pedrochacon9621@gmail.com>
2025-09-15 22:26:13 +0200

    o2

--- api/index.php
+++ api/index.php
@@ -1,6 +1,18 @@
 <?php
 require __DIR__ . '/../index.php';
+// Extrae la ruta solicitada
+$ruta = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+// Ruta base del proyecto
+$base = __DIR__ . '/..';
+
+// Verifica si el archivo solicitado existe
+$archivoSolicitado = $base . $ruta;
+
+if (file_exists($archivoSolicitado) && pathinfo($archivoSolicitado, PATHINFO_EXTENSION) === 'php') {
+    require $archivoSolicitado;
+    exit;
+}
 ?>
 
 
