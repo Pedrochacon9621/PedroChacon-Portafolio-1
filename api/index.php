33e3225d4978117ec7daf917ce9aaf4ef4e8de30
Pedro Chacón <pedrochacon9621@gmail.com>
2025-09-15 20:04:00 +0200

    configurando rutas paginas1

--- api/index.php
+++ api/index.php
@@ -1,3 +1,18 @@
 <?php
 require __DIR__ . '/../index.php';
+
+$ruta = $_SERVER['REQUEST_URI'];
+
+switch ($ruta) {
+    case '/paginas/politica.php':
+        require __DIR__ . '/paginas/politica.php';
+        break;
+    case '/sobre-mi':
+        require __DIR__ . '/paginas/sobre-mi.php';
+        break;
+    default:
+        require __DIR__ . '/componentes/404.php';
+        break;
+}
+
 ?>
