d0f68c269a94da152b1c7a303ef88c659dd3ae7f
Pedro Chacón <pedrochacon9621@gmail.com>
2025-09-15 21:08:23 +0200

    agregando mas componentes a poli

--- api/paginas/politica.php
+++ api/paginas/politica.php
@@ -2,4 +2,5 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/componentes/nav.php';
 require __DIR__ . '/../../paginas/politica.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/componentes/footer.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/componentes/modalContacto.php';
 ?>
