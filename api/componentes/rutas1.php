69420a73371be777a04440dd727ed501daa9c450
Pedro Chacón <pedrochacon9621@gmail.com>
2025-09-15 21:33:35 +0200

    agregando carpeta componentes a api

---
+++ api/componentes/rutas1.php
@@ -0,0 +1,3 @@
+<?php
+require __DIR__ . '/../../componentes/rutas1.php';
+?>
--- api/paginas/politica.php
+++ api/paginas/politica.php
@@ -1,7 +1,3 @@
 <?php
-require '/../../componentes/nav.php';
 require __DIR__ . '/../../paginas/politica.php';
-require '/../../componentes/footer.php';
-require '/../../componentes/modalContacto.php';
-
 ?>
