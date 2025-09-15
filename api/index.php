ec5c8b1972eba832e751aa74b2469da07c45fa22
Pedro Chacón <pedrochacon9621@gmail.com>
2025-09-15 20:25:49 +0200

    ruta6

--- api/index.php
+++ api/index.php
@@ -1,13 +1,19 @@
 <?php
 require __DIR__ . '/../index.php';
 
-// Detecta la ruta solicitada
-$ruta = $_SERVER['REQUEST_URI'];
+// Extrae solo la ruta sin parámetros
+$ruta = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-// Maneja la ruta de la política
-if ($ruta === '/paginas/politica.php') {
-    require __DIR__ . '/../paginas/politica.php';
-    exit;
+// Manejador de rutas
+switch ($ruta) {
+    case '/paginas/politica.php':
+        require __DIR__ . '/../paginas/politica.php';
+        exit;
+
+    // Puedes agregar más rutas aquí si lo necesitas
+    default:
+        // Si no coincide con ninguna ruta, sigue con index.php
+        break;
 }
 
 ?>
