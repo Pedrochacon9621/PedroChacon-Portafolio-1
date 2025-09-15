5a96a46054d0eccaa328b3f585670e2cdd589e81
Pedro Chacón <pedrochacon9621@gmail.com>
2025-09-15 22:30:08 +0200

    o4

--- api/index.php
+++ api/index.php
@@ -1,4 +1,13 @@
 <?php
+echo '<pre>';
+echo 'REQUEST_URI: ' . $_SERVER['REQUEST_URI'] . "\n";
+echo 'PATH_INFO: ' . ($_SERVER['PATH_INFO'] ?? 'N/A') . "\n";
+echo 'SCRIPT_NAME: ' . $_SERVER['SCRIPT_NAME'] . "\n";
+echo 'PHP_SELF: ' . $_SERVER['PHP_SELF'] . "\n";
+echo '</pre>';
+
+
+
 require __DIR__ . '/../index.php';
 // Extrae la ruta solicitada
 $ruta = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
@@ -13,6 +22,9 @@ if (file_exists($archivoSolicitado) && pathinfo($archivoSolicitado, PATHINFO_EXT
     require $archivoSolicitado;
     exit;
 }
+
+echo '<pre>Ruta limpia: ' . $ruta . '</pre>';
+
 ?>
 
 
