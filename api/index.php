56b4e21e575d28b9ada29eb5d47b8c6f16012658
Pedro Chacón <pedrochacon9621@gmail.com>
2025-09-15 22:23:04 +0200

    o1

--- api/index.php
+++ api/index.php
@@ -1,16 +1,5 @@
 <?php
 require __DIR__ . '/../index.php';
-$ruta = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-switch ($ruta) {
-    case '/paginas/politica.php':
-        require __DIR__ . '/../paginas/politica.php';
-        break;
-
-    default:
-        require __DIR__ . '/../index.php';
-        break;
-}
 
 ?>
 
