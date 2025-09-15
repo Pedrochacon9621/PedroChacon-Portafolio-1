fb1704284483ecbb172dd962bb8f70b8ec51ad2b
Pedro Chacón <pedrochacon9621@gmail.com>
2025-09-15 20:35:45 +0200

    agregando carpeta pagina en api

--- api/index.php
+++ api/index.php
@@ -1,19 +1,3 @@
 <?php
 require __DIR__ . '/../index.php';
-
-// Extrae solo la ruta sin parámetros
-$ruta = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-// Manejador de rutas
-switch ($ruta) {
-    case 'paginas/politica.php':
-        require __DIR__ . '/../paginas/politica.php';
-        exit;
-
-    // Puedes agregar más rutas aquí si lo necesitas
-    default:
-        // Si no coincide con ninguna ruta, sigue con index.php
-        break;
-}
-
 ?>
