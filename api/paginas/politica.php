e830b5d9ac1f32ee9953b504bfb8c28e0bba650f
Pedro Chacón <pedrochacon9621@gmail.com>
2025-09-15 21:24:30 +0200

    2

--- api/paginas/politica.php
+++ api/paginas/politica.php
@@ -1,8 +1,7 @@
 <?php
-require __DIR__ . '/../../paginas/politica.php';
 require __DIR__ . '/../../componentes/nav.php';
+require __DIR__ . '/../../paginas/politica.php';
 require __DIR__ . '/../../componentes/footer.php';
 require __DIR__ . '/../../componentes/modalContacto.php';
-
 
 ?>
