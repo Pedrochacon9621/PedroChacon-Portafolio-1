a8a371dfc919962026d8c28a3ed5bdc8112fdd99
Pedro Chacón <pedrochacon9621@gmail.com>
2025-09-15 20:41:03 +0200

    configurando rutas en politica1

--- api/paginas/politica.php
+++ api/paginas/politica.php
@@ -1,3 +1,6 @@
 <?php
+require $_SERVER['DOCUMENT_ROOT'] . '/componentes/nav.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/componentes/footer.php';
+
 require __DIR__ . '/../../paginas/politica.php';
 ?>
