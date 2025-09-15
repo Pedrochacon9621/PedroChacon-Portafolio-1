4de306d60f1604a123bcd3e900ef8216f705d3f3
Pedro Chacón <pedrochacon9621@gmail.com>
2025-09-15 20:43:19 +0200

    config rutas politica2

--- api/paginas/politica.php
+++ api/paginas/politica.php
@@ -1,6 +1,5 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/componentes/nav.php';
-require $_SERVER['DOCUMENT_ROOT'] . '/componentes/footer.php';
-
 require __DIR__ . '/../../paginas/politica.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/componentes/footer.php';
 ?>
