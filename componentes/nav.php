21d45a9b2d453292666bccd2e709dba733563cc3
Pedro Chacón <pedrochacon9621@gmail.com>
2025-10-03 15:40:55 +0200

    edit 03102025 01

--- componentes/nav.php
+++ componentes/nav.php
@@ -1,12 +1,10 @@
 <?php
     include "rutas1.php";
 ?>
-
 <nav class="nav1">
     <div class="contentHeader">
 <!-- Para agregar posible contenido -->
     </div>
-
     <div id="hamburger">
                 <img src="<?= $rutaImagenes ?>/icons/menu-2.svg" alt="">
                 <ul class="menu2">
@@ -17,9 +15,7 @@
                     <li><a href="<?= $rutaPolitica ?>">Políctica de Privacidad</a></li>
                 </ul>
     </div>
-
     <ul class="menu1">
-        
         <li class="logo"><a href="<?= $rutaRaiz ?>"><img src="<?= $rutaImagenes ?>/icons/casaGoogleBlanca.svg" alt=""></a></li>
         <li><a href="<?= $rutaTecnologias ?>">Tecnologías</a></li>
         <li> <a href="<?= $rutaProyectos ?>">Proyectos</a></li>
